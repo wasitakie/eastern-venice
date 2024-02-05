c584048b56a6c6edcbdf06beeddbf530bf66966d
koo21 <sawaddiju@hotmail.com>
2024-02-05 11:16:50 +0700

    up banner

--- pages/slide.php
+++ pages/slide.php
@@ -1,12 +1,10 @@
 <div class="container-fluid slideMain">
     <div class="single-item">
         <div class="slide">
-            <img src="https://scontent.fkdt3-1.fna.fbcdn.net/v/t39.30808-6/311521369_440217848219080_3278703255139881022_n.png?_nc_cat=107&ccb=1-7&_nc_sid=783fdb&_nc_ohc=hjxEk2ZQB_kAX_fDDNZ&_nc_ht=scontent.fkdt3-1.fna&oh=00_AfAvoz7yyWGGoyvdjbq00OSYO_9C8ZaszRSLk34tWULcHQ&oe=65BDEE71"
-                width="100%" alt="" srcset="">
+            <img src="../images/banner/b1.jpg" width="100%" alt="" srcset="">
         </div>
         <div class="slide">
-            <img src="https://images.unsplash.com/photo-1682687220161-e3e7388e4fad?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwxfHx8ZW58MHx8fHx8"
-                width="100%" alt="" srcset="">
+            <img src="../images/banner/b2.jpg" width="100%" alt="" srcset="">
         </div>
     </div>
 </div>
